26a5f4e3647b6dc2c5d7c6a387d86b5f8a9ca1c0
Richard Hua <hua.richard0@gmail.com>
2026-01-29 20:12:10 -0800

    Get Pokemon

---
+++ get-pokemon.r
@@ -0,0 +1,33 @@
+library(httr)
+library(jsonlite)
+
+res <- GET("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+stop_for_status(res)
+
+pokemon_list <- fromJSON(content(res, "text", encoding = "UTF-8"))$results[1:10,]
+
+get_stats <- function(url) {
+  res <- GET(url)
+  stop_for_status(res)
+
+  p <- fromJSON(content(res, "text", encoding = "UTF-8"))
+
+  stats <- setNames(
+    p$stats$base_stat,
+    p$stats$stat$name
+  )
+
+  list(
+    name    = p$name,
+    hp      = stats["hp"],
+    attack  = stats["attack"],
+    defense = stats["defense"],
+    moves = p$moves,
+    picture = p$sprites$other$`official-artwork`$front_default
+  )
+}
+
+pokemon_stats <- lapply(pokemon_list$url, get_stats)
+
+json_out <- prettify(toJSON(pokemon_stats, auto_unbox = TRUE, pretty = TRUE))
+writeLines(json_out, "pokemon_stats.json")
