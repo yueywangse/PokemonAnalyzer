7b7c0775632771d7e28102a17c648c9ddd98a82d
Richard Hua <hua.richard0@gmail.com>
2026-02-02 16:46:26 -0800

    Tests

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,13 +1,41 @@
-test_that("live call pulls pikachu when online", {
-  skip_if_offline(host = "pokeapi.co")
-  res <- pokeapi_get_pokemon("pikachu")
-  expect_equal(res$name, "pikachu")
-  expect_true("speed" %in% res$stats$stat)
+test_that("pokeapi_get_pokemon can run without HTTP", {
+  fake_request <- function(path, query = list(), ...) {
+    expect_match(path, "^pokemon/")
+    jsonlite::fromJSON(testthat::test_path("data/pikachu.json"), simplifyVector = FALSE)
+  }
+
+  with_mocked_in_namespace(
+    "PokemonAnalyzer",
+    pokeapi_request = fake_request,
+    {
+      res <- pokeapi_get_pokemon("pikachu")
+      expect_equal(res$name, "pikachu")
+      expect_true("speed" %in% res$stats$stat)
+    }
+  )
 })
 
-test_that("listing uses pagination", {
-  skip_if_offline(host = "pokeapi.co")
-  res <- pokeapi_list_pokemon(limit = 5, offset = 0)
-  expect_equal(nrow(res), 5)
-  expect_true(all(c("name", "id", "url") %in% names(res)))
+test_that("pokeapi_list_pokemon handles pagination without HTTP", {
+  fake_request <- function(path, query = list(), ...) {
+    expect_equal(path, "pokemon")
+    expect_equal(query$limit, 5)
+    expect_equal(query$offset, 0)
+    list(
+      results = lapply(1:5, function(i) list(
+        name = paste0("poke", i),
+        url = sprintf("https://pokeapi.co/api/v2/pokemon/%s/", i)
+      ))
+    )
+  }
+
+  with_mocked_in_namespace(
+    "PokemonAnalyzer",
+    pokeapi_request = fake_request,
+    {
+      res <- pokeapi_list_pokemon(limit = 5, offset = 0)
+      expect_equal(nrow(res), 5)
+      expect_true(all(c("name", "id", "url") %in% names(res)))
+      expect_equal(res$id, 1:5)
+    }
+  )
 })
